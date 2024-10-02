26d365d5259c44f2c28014a229573159cff63734
wenqi <WHan@smail.pembinatrails.ca>
2024-10-02 13:52:14 -0500

    Add monster feature to the game

--- game.sh
+++ game.sh
@@ -1,5 +1,4 @@
-subl game.sh
-echo "welcome to the Adventure Game!"
+echo "welcome to the Ultimate Adventure Game!"
 echo "choose a location to explore: forest, castle, or cave"
 read location
 if [ "$location" -- "forest" ]; then
